e9bc89e933b2432529656501a7ab9ef5a30612f4
Dan Maltbie <dmaltbie@daloma.org>
2022-01-06 23:04:58 -0800

    can now use python 3.8 to local install package

--- setup.py
+++ setup.py
@@ -3,10 +3,6 @@
 from setuptools import setup
 
 setup(
-#      name='tinyBT',
-#      description='Simple BitTorrent DHT',
-#      author='Fred Stober, Dan Maltbie (Packaging)',
-#      author_email='dmaltbie@daloma.org',
-#      setup_requires=['setup.cfg'],
-#      setup_cfg=True
-     )
+#    setup_requires=['setup.cfg'],
+#    setup_cfg=True
+)
